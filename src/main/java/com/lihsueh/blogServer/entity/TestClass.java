1d3bed029b2068a8937ae14d2e19b176353aa019
Li <aaa111334@gmail.com>
2024-11-09 23:09:42 +0800

    add TestClass and modified setting

---
+++ src/main/java/com/lihsueh/blogServer/entity/TestClass.java
@@ -0,0 +1,13 @@
+package com.lihsueh.blogServer.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+
+public class TestClass {
+
+    @Id
+    private long id;
+
+}
