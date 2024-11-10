e1210db43e31208e7a2b32999451109ca8bee65b
Li <aaa111334@gmail.com>
2024-11-10 18:06:28 +0800

    add get id service

--- src/main/java/com/lihsueh/blogServer/PostServiceImpl.java
+++ src/main/java/com/lihsueh/blogServer/PostServiceImpl.java
@@ -1,6 +1,8 @@
 package com.lihsueh.blogServer;
 
 import java.util.Date;
+import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,13 +11,15 @@ import com.lihsueh.blogServer.entity.Post;
 import com.lihsueh.blogServer.repository.PostRepository;
 import com.lihsueh.blogServer.service.PostService;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class PostServiceImpl implements PostService {
-    
+
     @Autowired
     private PostRepository postRepository;
 
-    public Post savePost(Post post){
+    public Post savePost(Post post) {
         post.setLikeCount(0);
         post.setViewCount(0);
         post.setDate(new Date());
@@ -23,5 +27,19 @@ public class PostServiceImpl implements PostService {
         return postRepository.save(post);
     }
 
+    public List<Post> getAllPosts() {
+        return postRepository.findAll();
+    }
+
+    public Post gePostById(Long postId){
+        Optional<Post> optionalPost = postRepository.findById(postId);
+        if(optionalPost.isPresent()){
+            Post post = optionalPost.get();
+            post.setViewCount(post.getViewCount() + 1);
+            return postRepository.save(post);
+        }else{
+            throw new EntityNotFoundException("並未找到貼文");
+        }
+    }
 
 }
--- src/main/java/com/lihsueh/blogServer/service/PostService.java
+++ src/main/java/com/lihsueh/blogServer/service/PostService.java
@@ -1,7 +1,15 @@
 package com.lihsueh.blogServer.service;
 
+import java.util.List;
+
 import com.lihsueh.blogServer.entity.Post;
 
 public interface PostService {
+    
     Post savePost(Post post);
+
+    public List<Post> getAllPosts();
+
+    Post gePostById(Long postId);
+
 }
